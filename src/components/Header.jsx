b0e08024e9261f439f83200b37509c448621d3cd
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-06-18 13:39:11 -0600

    Add animation to nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <div className="flex justify-around">
+    <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between w-full gap-4 sm:gap-0 px-10 sm:px-36 h-20 pt-10 sm:pt-0">
       LOGO
       <Navbar />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <ul>
+    <ul className="flex gap-8 items-center">
       <li>
-        <Link to="about" smooth={true} duration={500}>
+        <Link to="about" smooth={true} duration={500} className="nav-link">
           About
         </Link>
       </li>
       <li>
-        <Link to="projects" smooth={true} duration={500}>
+        <Link to="projects" smooth={true} duration={500} className="nav-link">
           Projects
         </Link>
       </li>
       <li>
-        <Link to="contact" smooth={true} duration={500}>
+        <Link to="contact" smooth={true} duration={500} className="nav-link">
           Contact me
         </Link>
       </li>
