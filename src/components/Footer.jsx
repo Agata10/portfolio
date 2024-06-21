79c58cada66c6af7ee775db712a8a62677eaed01
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-06-21 10:55:50 -0600

    Add logo to the window bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center mt-auto h-12 py-8 text-sm font-main">
+    <div className="flex justify-center items-center mt-auto h-12 py-8 text-sm font-main bg-darker">
       Agata Deter &copy; Copyright 2024
     </div>
   );
