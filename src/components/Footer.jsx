85fbcad4a4b9a4d3f6d9c7d14376910ae1d708f1
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-06-21 08:56:03 -0600

    Style footer font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center mt-auto h-12 py-8">
+    <div className="flex justify-center items-center mt-auto h-12 py-8 text-sm font-main">
       Agata Deter &copy; Copyright 2024
     </div>
   );
