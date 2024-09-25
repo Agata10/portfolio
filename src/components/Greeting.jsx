1d9a3ec7a6cf89f837a4cd9289200c41c7bc842c
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-09-25 14:01:03 -0600

    Change greeting section

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -11,9 +11,11 @@ const Greeting = () => {
           I'm a full-stack developer.
         </h6>
         <p className="w-full sm:w-10/12 leading-8 sm:leading-8 font-par text-lg">
-          Creative proffesional passionate about designing appealing and
-          user-friendly websites. Ready and excited to transition into the world
-          of IT.
+          As a passionate engineer, I bring creative ideas and technical
+          expertise to design and develop intuitive, user-focused web
+          applications. With a strong foundation in both front and back-end
+          development, I'm eager to transform innovative solutions into
+          functional and impactful software.
         </p>
       </div>
       <div>
