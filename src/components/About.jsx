6bdf241491874781d4f35318cad10fc2a1969be6
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-09-25 14:05:21 -0600

    Change about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,19 +9,19 @@ const About = ({ clicked }) => {
     >
       <div className="w-10/12 sm:w-9/12 mt-10 sm:mt-24">
         <h2 className="text-3xl text-or font-nav text-left py-4">About me</h2>
-        <p className="font-par w-full sm:w-6/12 text-base leading-8 pb-6">
-          I am a passionate about technology since my Control Engineering
-          studies. My educational journey has given me a solid foundation in
-          programming and developed my passion for IT. I enjoy creating
-          appealing websites that prioritize both creativity and user
-          experience.
+        <p className="font-par w-full md:w-6/12 text-base leading-8 pb-6">
+          My passion for technology began during my Automation Control
+          Engineering and Robotics studies, where I built a solid foundation in
+          programming and discovered my love for IT. I thrive on designing
+          websites that balance creativity and user experience, ensuring each
+          project is both functional and visually engaging.
         </p>
-        <p className="font-par w-full sm:w-6/12 text-base leading-8 pb-6">
-          As a recent graduate of a 15-week Software Engineering Bootcamp, I am
-          looking forward to joining a company that will allow me to grow and
-          continue developing my passion for the tech industry.
+        <p className="font-par w-full md:w-6/12 text-base leading-8 pb-6">
+          As a recent graduate of a 15-week Software Engineering Bootcamp, I'm
+          excited to contribute to a forward-thinking company that fosters
+          growth and fuels my enthusiasm for the tech industry.
         </p>
-        <div className="font-par w-full sm:w67/12 text-base leading-8">
+        <div className="font-par w-full md:w-6/12 text-base leading-8">
           Here are some of the latest technologies I've been working with:
           <div className="flex gap-6 sm:gap-10 pt-2">
             <ul className="list-arrow text-sm font-main">
